42843e873df01a98daa35cace873f972c928ff45
Redouane Achouri <redouane.a.achouri@gmail.com>
2023-03-21 05:44:03 +0100

    Log out viewed page name

--- wiki.go
+++ wiki.go
@@ -31,6 +31,9 @@ func loadPage(title string) (*Page, error) {
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
+
+	fmt.Println("Serving page:", title)
+
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
